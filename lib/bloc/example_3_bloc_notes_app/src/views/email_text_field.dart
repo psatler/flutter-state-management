49951454b111df3828ca33e7c92cd7d951295e95
Pablo Satler <pablosatler@gmail.com>
2022-06-25 20:09:08 -0300

    chore: create text field for login form

---
+++ lib/bloc/example_3_bloc_notes_app/src/views/email_text_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_management/bloc/example_3_bloc_notes_app/src/strings.dart';
+
+class EmailTextField extends StatelessWidget {
+  const EmailTextField({
+    super.key,
+    required this.emailController,
+  });
+
+  final TextEditingController emailController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: const InputDecoration(
+        hintText: enterYourEmailHere,
+      ),
+    );
+  }
+}
---
+++ lib/bloc/example_3_bloc_notes_app/src/views/password_text_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_management/bloc/example_3_bloc_notes_app/src/strings.dart';
+
+class PasswordTextField extends StatelessWidget {
+  const PasswordTextField({
+    super.key,
+    required this.passwordController,
+  });
+
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: passwordController,
+      // keyboardType: TextInputType.visiblePassword,
+      obscureText: true,
+      obscuringCharacter: '◉',
+      decoration: const InputDecoration(
+        hintText: enterYourPasswordHere,
+      ),
+    );
+  }
+}
